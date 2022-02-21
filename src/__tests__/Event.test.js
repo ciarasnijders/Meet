7a2c652b31663ea8d27e3c527e0f05ddfced3a4f
Ciara <ciarazsnijders@gmail.com>
2022-02-21 21:26:44 +0100

    Create Event.test.js

---
+++ src/__tests__/Event.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import  Event  from '../Event';
+import { mockData } from '../mock-data';
+
+describe('<Event /> Component', () => {
+    let EventWrapper;
+
+    beforeAll(() => {EventWrapper = shallow(<Event event={mockData[1]} />);
+    }); 
+
+    test('render event', () => {
+        expect(EventWrapper.find('.event')).toHaveLength(1);
+    });
+
+    test('render event summary', () => {
+        expect(EventWrapper.find('.summary')).toHaveLength(1);
+    });
+
+    test('render event location', () => {
+        expect(EventWrapper.find('.location')).toHaveLength(1);
+    });
+
+    test('render event show details button', () => {
+        expect(EventWrapper.find('.details-btn')).toHaveLength(1);
+    });
+
+    test('show details when button clicked', () => {
+        EventWrapper.setState({
+            collapsed:true
+        });
+        EventWrapper.find('.details-btn').simulate('click');
+        expect(EventWrapper.state('collapsed')).toBe(false);
+    });
+
+    test('hide details when button clicked', () => {
+        EventWrapper.setState({
+            collapsed:false
+        });
+        EventWrapper.find('.details-btn').simulate('click');
+        expect(EventWrapper.state('collapsed')).toBe(true);
+    });
+
+});
