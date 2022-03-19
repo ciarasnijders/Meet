a10e4c35ef05eefafe98e279a4643bdbc18e6563
Ciara <ciarazsnijders@gmail.com>
2022-03-19 18:56:22 +0100

    4.9 add scatterchart

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import { extractLocations, getEvents, checkToken, getAccessToken } from './api';
 import './App.css';
 import './nprogress.css';
 import WelcomeScreen from './WelcomeScreen';
+import {
+  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip
+} from 'recharts';
 
 
 
@@ -63,16 +66,28 @@ class App extends Component {
     this.updateEvents(this.state.currentLocation)
   };
 
+  getData = () => {
+    const {locations, events} = this.state;
+    const data = locations.map((location)=>{
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(', ').shift()
+      return {city, number};
+    })
+    return data;
+  };
+
 
   render() {
-    
     if (this.state.showWelcomeScreen === undefined) return 
     <div
       className="App" 
     />
+    const { locations, numberOfEvents } = this.state;
 
     return (
       <div className="App">
+        <h1>Meet App</h1>
+        <h4>Choose your nearest city</h4>
         <CitySearch 
           locations={this.state.locations} 
           updateEvents={this.updateEvents} 
@@ -83,6 +98,23 @@ class App extends Component {
           numberOfEvents={this.state.NumberOfEvents}
           errorText={this.state.errorText}
         />
+
+        <h4>Events in each city</h4>
+
+        <ScatterChart
+        width={400}
+        height={400}
+        margin={{
+          top: 20, right: 20, bottom: 20, left: 20,
+        }}
+        >
+        <CartesianGrid />
+        <XAxis type="category" dataKey="city" name="City" />
+        <YAxis type="number" dataKey="number" name="Number of Events"/>
+        <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+        <Scatter data={this.getData()} fill="#8884d8" />
+        </ScatterChart>
+
         <EventList 
           events={this.state.events}
           numberOfEvents={this.state.NumberOfEvents} 
