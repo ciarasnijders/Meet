6df3b5aa9eda8e7b0a3077ed0b48eef2b89cd50d
Ciara <ciarazsnijders@gmail.com>
2022-03-19 18:10:47 +0100

    4.8 add warning when user offline and using cashe

--- src/EventList.js
+++ src/EventList.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import Event from './Event';
+import { Container } from 'react-bootstrap';
+import { WarningAlert } from './Alert';
 
 class EventList extends Component {
   render() {
     const { events } = this.props;
     return (
-      <ul className="EventList">
-        {events.map(event =>
-          <li key={event.id}>
-            <Event event={event} />
-          </li>
-        )}
-      </ul>
+      <Container>
+        <div>
+        {!navigator.onLine ? (
+						<WarningAlert text="You are offline, the events list has been loaded from the Cache" />
+					) : ('')
+        }
+
+        <ul className="EventList">
+          {events.map(event =>
+            <li key={event.id}>
+              <Event event={event} />
+            </li>
+          )}
+        </ul>
+
+        </div>
+      </Container>
     );
   }
 }
